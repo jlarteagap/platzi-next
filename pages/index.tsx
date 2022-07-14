81c9d6b9bad4c209e74949efe393dfb36d331bca
Jorge <jorgelarteagap@gmail.com>
2022-07-13 22:43:39 -0400

    Product list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import data from 'database/data'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((res) => res.json())
+      .then(({ data, length }) => {
+        setProductList(data)
+      })
+  }, [])
   return (
     <div>
       <Navbar />
-      <div>Platzi and Next.js!</div>
+      <div>Platzi and Next.jss!</div>
+      {productList.map((product) => (
+        <div>{product.name}</div>
+      ))}
     </div>
   )
 }
